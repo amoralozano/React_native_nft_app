bced395322924bcdbbf7e0a9110d787101441753
abdael mora <azmoracrads@gmail.com>
2022-04-21 01:01:41 -0700

    home page vDemo done, details page started

--- screens/Details.js
+++ screens/Details.js
@@ -1,7 +1,71 @@
-import { Text } from "react-native";
+import {
+  Text,
+  View,
+  SafeAreaView,
+  Image,
+  StatusBar,
+  FlatList,
+} from "react-native";
+import { COLORS, SIZES, SHADOWS, FONTS, assets } from "../constants";
+import {
+  CircleButton,
+  RectButton,
+  SubInfo,
+  FocusedStatusBar,
+  DetailsDesc,
+  DetailsBid,
+} from "../components";
+import React from "react";
 
-const Details = () => {
-  return <Text>Details</Text>;
+const DetailsHeader = ({ data, navigation }) => (
+  <View style={{ width: "100%", height: 373 }}>
+    <Image
+      source={data.image}
+      resizeMode="cover"
+      style={{ width: "100%", height: "100%" }}
+    />
+  </View>
+);
+
+const Details = ({ route, navigation }) => {
+  const { data } = route.params;
+
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <FocusedStatusBar
+        barStyle="dark-content"
+        backgroundColor="transparent"
+        transLucent={true}
+      />
+      <View
+        style={{
+          width: "100%",
+          position: "absolute",
+          bottom: 0,
+          paddingVertical: SIZES.font,
+          justifyContent: "center",
+          alignItems: "center",
+          backgroundColor: "rgba(255,255,255,0.5)",
+          zIndex: 1,
+        }}
+      >
+        <RectButton minWidth={170} fontSize={SIZES.large} {...SHADOWS.dark} />
+      </View>
+
+      <FlatList
+        data={data.bids}
+        renderItem={({ item }) => <DetailsBid bid={item} />}
+        keyExtractor={(item) => item.id}
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ paddingBottom: SIZES.extraLarge * 3 }}
+        ListHeaderComponent={() => (
+          <React.Fragment>
+            <DetailsHeader data={data} navigation={navigation} />
+          </React.Fragment>
+        )}
+      />
+    </SafeAreaView>
+  );
 };
 
 export default Details;
